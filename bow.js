a499c8f3f01bab3326b3fc467b7a172296cea988
BeigeWoods <juhyunkbstar@gmail.com>
2022-12-26 22:51:12 +0900

    충돌 해결

--- bow.js
+++ bow.js
@@ -1,9 +1,5 @@
 print("hello world");
-<<<<<<< HEAD
 // conflict1 내용
 print("I dont't wanna conflict!");
 // main 내용
-=======
-print("for conflict?");
->>>>>>> f0f9e6bde5006ede43ff13188f5c8126a0bd7260
 print("for conflict!");
